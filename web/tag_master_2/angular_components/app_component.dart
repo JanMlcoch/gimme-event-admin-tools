dba472e70f07b8668753aec6b09e921cf9423bab
GrundL <lubomir.grund+gitlab@gmail.com>
2016-11-29 19:52:18 +0100

    [TM] angular first steps part II - angular router

--- web/tag_master_2/angular_components/app_component.dart
+++ web/tag_master_2/angular_components/app_component.dart
@@ -2,21 +2,27 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 import 'package:angular2_components/angular2_components.dart';
 
 @Component(
     selector: 'my-app',
     styleUrls: const ['app_component.css'],
     templateUrl: 'app_component.html',
-    directives: const[materialDirectives],
-    providers: const [materialProviders])
-class AppComponent {
-  String path1 = "path1";
-  String path2 = "path2";
-  String path3 = "path3";
-  String path = "";
+    directives: const [materialDirectives, ROUTER_DIRECTIVES],
+    providers: const [materialProviders, ROUTER_PROVIDERS])
+@RouteConfig(const [
+  const Route(path: '/path1', name: 'Path1', component: Component1),
+  const Route(path: '/path2', name: 'Path2', component: Component2),
+  const Route(path: '/path3', name: 'Path3', component: Component3),
+])
+class AppComponent {}
 
-  void setPath(String newPath) {
-    path = newPath;
-  }
-}
+@Component(selector: "component1", template: 'Hello1')
+class Component1 {}
+
+@Component(selector: "component2", template: 'Hello2')
+class Component2 {}
+
+@Component(selector: "component3", template: 'Hello3')
+class Component3 {}
