b62ec7c0b0103516feb542837f34a5a7ec2e9240
GrundL <lubomir.grund+gitlab@gmail.com>
2016-12-04 13:29:48 +0100

    [TM] minor test features

--- test/tag_master/repo_diff.dart
+++ test/tag_master/repo_diff.dart
@@ -1,8 +1,18 @@
 part of test.tagMaster2;
 
 void repoDiffTests() {
-//  test("Test of empty-empty diff", () {
-//    List<Record> diff = Record.diff(emptyRepo,emptyRepo);
-//    expect(diff, equals(expectedRepo.toMap()));
-//  });
+  test("Test of empty-empty diff", () {
+    List<Record> diff = Record.diff(emptyRepo,emptyRepo);
+    expect(diff, equals([]));
+  });
+
+  test("Test of tag-tag diff", () {
+    List<Record> diff = Record.diff(repoOnlyCustomTag,repoOnlyCustomTag);
+    expect(diff, equals([]));
+  });
+
+  test("Test of empty-tag diff", () {
+    List<Record> diff = Record.diff(emptyRepo,repoOnlyCustomTag);
+    expect(diff, equals([]));
+  });
 }
