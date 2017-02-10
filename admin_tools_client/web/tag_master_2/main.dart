2706eb46e07485d57cdf56cd68728b202227bfdd
Jan Mlcoch <mlcoch.jan@gmail.com>
2017-02-10 16:21:09 +0100

    fix client index.html

--- admin_tools_client/web/tag_master_2/main.dart
+++ admin_tools_client/web/tag_master_2/main.dart
@@ -2,28 +2,11 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-//import 'package:angular2/platform/browser.dart';
-//import 'package:admin_tools/tag_master/angular_components/app_component.dart';
+import 'package:angular2/platform/browser.dart';
+import 'package:admin_tools/tag_master/angular_components/app_component.dart';
 
 void main() {
-//  bootstrap(AppComponent);
-  start().then((dynamic a) => print("dsfsf"));
-  start().then((dynamic a) => print("dsfsf"));
-  start().then((dynamic a) => print("dsfsf"));
-  start().then((dynamic a) => print("dsfsf"));
-
-}
-
-Future start() async{
-  count();
-}
-
-int factorial(int n) => n == 0 ? 1 : n * factorial(n - 1);
-
-void count(){
-  for (int i = 0; i <= 8000; ++i) {
-    factorial(i);
-  }
+  bootstrap(AppComponent);
 }
 
 
