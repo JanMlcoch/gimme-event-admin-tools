33d97af399bb1a6c5aeedc1a48f3229e636bba37
Jan Mlcoch <mlcoch.jan@gmail.com>
2017-02-11 00:15:34 +0100

    Create new application Tagger for adding tags into foreign events

---
+++ admin_tools_client/lib/tagger/app_component.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+import 'package:admin_tools/tagger/events_list_component.dart';
+import 'package:admin_tools/tagger/event_component.dart';
+import 'package:admin_tools/tagger/services/events_service.dart';
+
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    styleUrls: const ['app_component.css'],
+    directives: const [
+      materialDirectives,
+      ROUTER_DIRECTIVES,
+      EventsListComponent
+    ],
+    providers: const [
+      materialProviders,
+      ROUTER_PROVIDERS
+    ])
+@RouteConfig(const [
+  const Route(path: '/', name: 'Empty', component: EmptyComponent),
+  const Route(path: '/event/:id', name: 'Event', component: EventDetailComponent),
+])
+class AppComponent{
+  final EventsService _eventsService;
+
+  AppComponent(this._eventsService);
+
+}
+
+
+@Component(
+    selector: 'empty',
+    template: '',
+    directives: const [],
+    providers: const [])
+class EmptyComponent{}
---
+++ admin_tools_client/lib/tagger/services/events_service.dart
@@ -0,0 +1,32 @@
+
+import 'package:angular2/core.dart';
+
+import 'package:admin_tools/tagger/model/library.dart';
+
+@Injectable()
+class EventsService {
+  Events events;
+
+  EventsService(){
+    createMockEvents();
+  }
+
+  void createMockEvents(){
+    Map event1Data = {
+      "name": "Akce",
+      "dateFrom": 12345531353,
+      "image": "image",
+      "place": "Moje místo",
+      "annotation": "Moje anotace sdfkljasflksdmf klasmdfkl asdmfkl asdmdfl ",
+      "description": "Moje description dslfkjasdflkasdmn dlcfkasdm flkdsm fklmasd klfsdl kfklsj fsdkjf klsdjflk asjdfl",
+      "sourceUrl": "http://www.ksdfaslfks.sdkfljas/lsdkfjasdlkf"
+    };
+    Map event2Data = new Map.from(event1Data);
+    event2Data["name"] = "Moje jiná akce";
+
+    Event event1 = new Event()..fromMap(event1Data);
+    Event event2 = new Event()..fromMap(event2Data);
+    events = new Events();
+    events.list.addAll([event1, event2]);
+  }
+}
