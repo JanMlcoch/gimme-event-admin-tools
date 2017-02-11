76f3c9d5f643ab69cf9d13435b35510bf322c92e
GrundL <lubomir.grund+gitlab@gmail.com>
2017-02-11 17:03:32 +0100

    [Resolver] implementation part I

---
+++ admin_tools_client/lib/resolver/computor/library.dart
@@ -0,0 +1,17 @@
+library sidos.computor;
+
+import 'dart:math' as math;
+import 'package:admin_tools/sidos_entities/library.dart';
+import 'package:admin_tools/tag_master/library.dart';
+import 'package:admin_tools/resolver/resolver_interface/tag_master_repository_getter/tag_master_import_map_manual.dart' as tagMaster;
+
+//todo: more division
+
+part 'cachor/cachor.dart';
+part 'fittor/fittor.dart';
+part 'fittor/gaussian.dart';
+part 'imprintificator/imprintificator.dart';
+part 'patternificator/patternificator.dart';
+
+const num EUR_PER_KILOMETER = 0.03;
+const num MILLISECONDS_PER_KILOMETER = 43200;
